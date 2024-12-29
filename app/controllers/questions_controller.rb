8c5ecc021666b1d9b76d1fadff5ecefbaa76dbc1
daisy oliani <daisy.oliani@gmail.com>
2024-12-29 16:22:19 +0100

    add question funcitional turbo frame

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,24 +5,24 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @activity = Activity.find(params[:activity_id])
-    @question = @activity.questions.build(question_params)
+  @activity = Activity.find(params[:activity_id])
+  @question = @activity.questions.build(question_params)
 
-    if params[:question][:options].present?
-      @question.options = params[:question][:options].split(",").map(&:strip)
-    end
+  if params[:question][:options].present?
+    @question.options = params[:question][:options].split(",").map(&:strip)
+  end
 
-    if @question.save
-      respond_to do |format|
-        format.html { redirect_to activity_path(@activity), notice: "Pergunta criada com sucesso!" }
-        format.turbo_stream
-      end
-    else
-      respond_to do |format|
-        format.html { render :new, status: :unprocessable_entity }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace("question_form", partial: "questions/form", locals: { question: @question }) }
-      end
+  if @question.save
+    respond_to do |format|
+      format.html { redirect_to activity_path(@activity), notice: "Question added successfully!" }
+      # Eliminar o turbo_stream se deseja redirecionar completamente
     end
+  else
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.replace("question_form", partial: "questions/form", locals: { question: @question, activity: @activity }) }
+      format.html { render :new, status: :unprocessable_entity }
+    end
+  end
   end
 
   
@@ -31,5 +31,5 @@ class QuestionsController < ApplicationController
   def question_params
     params.require(:question).permit(:content, :options, :correct_answer)
   end  
-end 
+end
 
