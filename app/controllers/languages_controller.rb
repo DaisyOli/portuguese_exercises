f1b9672e84c5917257a91e6182de3bb1ea486bc8
daisy oliani <daisy.oliani@gmail.com>
2025-02-22 18:46:26 +0100

    switch languages ok size ok

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -5,10 +5,10 @@ class LanguagesController < ApplicationController
     if current_user.update(language: new_language)
       session[:locale] = new_language
       I18n.locale = new_language.to_sym
-      flash[:notice] = "Idioma alterado com sucesso!"
+      flash[:notice] = t('messages.language_updated')
       redirect_to request.referer || root_path, allow_other_host: false
     else
-      flash[:alert] = "Não foi possível alterar o idioma."
+      flash[:alert] = t('messages.language_update_failed')
       redirect_to request.referer || root_path, allow_other_host: false
     end
   end
