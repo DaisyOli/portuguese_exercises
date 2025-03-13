b439efb3c59e92ee1a0af8265c2e01b33e72f8d1
daisy oliani <daisy.oliani@gmail.com>
2025-03-13 19:12:47 +0100

    second attempt invitation_token on set_locale after authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale
+  before_action :set_locale, if: :user_signed_in?
   around_action :switch_locale
 
   def after_sign_in_path_for(resource)
