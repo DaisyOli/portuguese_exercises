14e3dcbd0d56d1bb2247f47bdb0b3ba84dccfc9e
daisy oliani <daisy.oliani@gmail.com>
2025-03-29 19:12:50 +0100

    Fix convite: adiciona params permitidos e campo language no form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :name, :language])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:role, :name, :language])
+    devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name, :language])
+    devise_parameter_sanitizer.permit(:invite, keys: [:name, :role, :language, :email])
   end
 
   private
