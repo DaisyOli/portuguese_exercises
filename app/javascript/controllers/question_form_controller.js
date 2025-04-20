5baf24e5bcfcfa1c3ca0480abae62b25c3a0e8d8
daisy oliani <daisy.oliani@gmail.com>
2025-04-20 18:41:21 +0200

    Corrigido formulário de questões após remoção da biblioteca Sortable

--- app/javascript/controllers/question_form_controller.js
+++ app/javascript/controllers/question_form_controller.js
@@ -9,7 +9,8 @@ export default class extends Controller {
     "fillInBlankHelp",
     "orderSentencesHelp",
     "orderSentencesFields",
-    "correctAnswerField"
+    "correctAnswerField",
+    "form"
   ];
 
   connect() {
